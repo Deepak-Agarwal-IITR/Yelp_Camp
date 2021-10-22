d6689e700da7cafcd01e77e25ab98d7b8cda4c5b
Deepak Agarwal <deepakjsagarwal@gmail.com>
2021-10-22 23:16:28 +0530

    Added public directory and express.static in app.js

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const reviews = require('./routes/reviews');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     //useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    //useFindAndModify: false
 });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
@@ -27,7 +28,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true }))
 app.engine('ejs', ejsMate);
-
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews)
