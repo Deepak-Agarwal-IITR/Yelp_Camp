1881385b4c2917ebe9eeb2986704f0d652070cfc
Deepak Agarwal <deepakjsagarwal@gmail.com>
2021-10-23 08:23:04 +0530

    Added express-session

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ const ejsMate = require('ejs-mate');
 
 const ExpressError = require('./utils/ExpressError');
 
-
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
 
+const session = require('express-session'
+)
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     //useCreateIndex: true,
@@ -29,6 +30,18 @@ app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true }))
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,'public')))
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now()+1000*60*60*24*7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews)
