5f322f9ac0160eb91d6c51406914387c853bcb8c
Deepak Agarwal <deepakjsagarwal@gmail.com>
2021-10-28 17:44:53 +0530

    Distributed campground routes to controllers but createReview  controllers but createReview controller not working

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,62 +5,16 @@ const catchAsync = require('../utils/catchAsync');
 //const ExpressError = require('../utils/ExpressError');
 //const { campgroundSchema } = require('../schemas.js');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const campgrounds = require('../controllers/campgrounds')
 
-router.get('/', catchAsync(async (req, res, next) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds });
-}))
-router.get('/new', isLoggedIn, (req, res) => {
-    res.render('campgrounds/new');
-})
-router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
-    //if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-    const campground = new Campground(req.body.campground);
-    campground.author = req.user._id;
-    //console.log(campground);
-    await campground.save();
+router.get('/', catchAsync(campgrounds.index))
+router.get('/new', isLoggedIn, campgrounds.renderNewForm)
+router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+router.get('/:id', catchAsync(campgrounds.showCampground))
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
 
-    req.flash('success', 'Successully made a new campground')
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-router.get('/:id', catchAsync(async (req, res, next) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id).populate({
-        path: 'reviews',
-        populate: {
-            path: 'author'
-        }
-    }).populate('author');
-   // console.log(campground);
-    if (!campground) {
-        req.flash('error', 'Cannot find that campground');
-        return res.redirect('/campgrounds');    // Dont know why used return without return it is working
-    }
-    res.render('campgrounds/show', { campground });
-}))
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res, next) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if (!campground) {
-        req.flash('error', 'Cannot find that campground');
-        return res.redirect('/campgrounds');
-    }
-    res.render('campgrounds/edit', { campground });
-}))
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res, next) => {
-    const { id } = req.params;
-    // console.log(req.body);
+router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
 
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    req.flash('success', 'Successfully updated campground');
-    res.redirect(`/campgrounds/${id}`)
-}))
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res, next) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    req.flash('success', 'Successfully deleted Campground')
-    res.redirect('/campgrounds');
-}))
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
--- routes/reviews.js
+++ routes/reviews.js
@@ -8,26 +8,9 @@ const catchAsync = require('../utils/catchAsync');
 //const ExpressError = require('../utils/ExpressError');
 //const { reviewSchema } = require('../schemas.js');
 const { validateReview,isLoggedIn,isReviewAuthor } = require('../middleware');
+const reviews = require('../controllers/reviews')
 
-router.post('/', isLoggedIn,validateReview, catchAsync(async (req, res) => {
-    // Now the id is in app.js so here we can not acces it without MERGEPARAMS = TRUE in router;
-    //console.log(req.params);
-    const camp = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    camp.reviews.push(review);
-    await review.save();
-    await camp.save();
-    req.flash('success','Created new review!')
-    res.redirect(`/campgrounds/${camp._id}`)
-}))
-router.delete('/:reviewId', isLoggedIn , isReviewAuthor ,catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
+router.post('/', isLoggedIn,validateReview, catchAsync(reviews.createReview))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
 
-    const campground = await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    const review = await Review.findByIdAndDelete(reviewId);
-    req.flash('success','Successfully deleted review')
-    res.redirect(`/campgrounds/${id}`);
-    //res.send("Deleted")
-}))
 module.exports = router;
