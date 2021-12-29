2f95f4de4a544b65a8a666f54178ef5260ec5c73
Deepak Agarwal <deepakjsagarwal@gmail.com>
2021-12-29 17:41:39 +0530

    Added cluster maps and fixed seeds till L555

--- models/campground.js
+++ models/campground.js
@@ -10,6 +10,8 @@ ImageSchema.virtual('thumbnail').get(function(){
     return this.url.replace('/upload', '/upload/w_200');
 });
 
+const opts = {toJSON:{virtuals:true}};
+
 const CampgroundSchema = new Schema({
     title: String,
     price: Number,
@@ -35,8 +37,11 @@ const CampgroundSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'Review'
     }]
-});
+},opts);
 
+CampgroundSchema.virtual('properties.popUpMarkup').get(function(){
+    return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>`;
+});
 CampgroundSchema.post('findOneAndDelete', async function(doc) {
     //console.log("Deleted");
     //console.log(doc);
