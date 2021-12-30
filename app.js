0ab1bd923bdd389d56c0d6fe3d24361397b24bfa
Deepak Agarwal <deepakjsagarwal@gmail.com>
2021-12-30 16:31:37 +0530

    Handled security issues till L569

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ const flash = require('connect-flash')
 const passport = require('passport')
 const LocalStrategy = require('passport-local');
 const User = require('./models/user')
+
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     //useCreateIndex: true,
@@ -40,13 +43,21 @@ app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true }))
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,'public')))
+app.use(
+    mongoSanitize({
+      replaceWith: '_',
+    }),
+);
+app.use(helmet());
 
 const sessionConfig = {
+    name: 'session',
     secret: 'thisshouldbeabettersecret',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+    //  secure: true,
         expires: Date.now()+1000*60*60*24*7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
@@ -54,6 +65,49 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash())
 
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/deepakagarwal/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 app.use(passport.initialize())
 // app.use(passport.session()) must be used after app.use(session(sessionConfig))
